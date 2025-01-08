7cf8487cd8fef5c8f2fc470b988a9b3f3e4bd4fe
SeohyunLilyChoi <lily_choi7@snu.ac.kr>
2025-01-09 04:24:47 +0900

    Change window.location.href to navigate

--- src/routes/Redirect.tsx
+++ src/routes/Redirect.tsx
@@ -1,20 +1,21 @@
 import { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Redirect = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-  const token = searchParmas.get('token')?.trim();
+  const token: string | undefined = searchParams.get('token')?.trim();
 
   useEffect(() => {
-    if (token !== null && token !== '') {
-      // token이 null이 아니고 빈 문자열도 아닌 경우
+    if (typeof token === 'string' && token !== '') {
+      // token이 string 타입이고 빈 문자열이 아닌 경우
       localStorage.setItem('token', token); // 로컬 스토리지에 저장
-      window.location.href = '/'; // 메인 페이지로 리다이렉트
+      void navigate('/'); // navigate 호출
     } else {
       console.error('Token not found or is empty in URL');
     }
-  }, [token]);
+  }, [token, navigate]);
 
   return <h1>Redirect</h1>;
 };
