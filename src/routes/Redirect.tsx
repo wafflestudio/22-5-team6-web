3cc48691814621431b74dd4df55e44f8e6c22a75
SeohyunLilyChoi <142142496+SeohyunLilyChoi@users.noreply.github.com>
2025-01-09 21:47:28 +0900

    Update src/routes/Redirect.tsx
    Co-authored-by: Hyunmin Woo (Hanul) <woohm404@gmail.com>

--- src/routes/Redirect.tsx
+++ src/routes/Redirect.tsx
@@ -4,17 +4,17 @@ import { useSearchParams } from 'react-router-dom';
 const Redirect = () => {
   const [searchParams] = useSearchParams();
 
-  useEffect(() => {
-    const token = searchParams.get('token'); // 쿼리 파라미터에서 token 추출
+  const token = searchParmas.get('token')?.trim();
 
-    if (token !== null && token.trim() !== '') {
+  useEffect(() => {
+    if (token !== null && token !== '') {
       // token이 null이 아니고 빈 문자열도 아닌 경우
       localStorage.setItem('token', token); // 로컬 스토리지에 저장
       window.location.href = '/'; // 메인 페이지로 리다이렉트
     } else {
       console.error('Token not found or is empty in URL');
     }
-  }, [searchParams]);
+  }, [token]);
 
   return <h1>Redirect</h1>;
 };
