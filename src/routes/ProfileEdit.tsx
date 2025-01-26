78d5f65937e04c037f5316e6e1051a4769c144f9
SeohyunLilyChoi <lily_choi7@snu.ac.kr>
2025-01-27 03:16:34 +0900

    Fix/fix profile edit page UI

--- src/routes/ProfileEdit.tsx
+++ src/routes/ProfileEdit.tsx
@@ -1,7 +1,16 @@
+import Footer from '@/components/home/Footer';
+import Header from '@/components/home/Topbar/Header';
 import ProfileEditForm from '@/components/user/profile/ProfileEditForm';
 
 const ProfileEdit = () => {
-  return <ProfileEditForm />;
+  return (
+    <>
+      <Header />
+      <hr className="w-full mb-8 border-t border-gray-300" />
+      <ProfileEditForm />
+      <Footer />
+    </>
+  );
 };
 
 export default ProfileEdit;
