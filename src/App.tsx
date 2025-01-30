bd5b90ff0c3ad328ba375779bab1a7940ade4d27
SeohyunLilyChoi <lily_choi7@snu.ac.kr>
2025-01-30 23:29:02 +0900

    Feat/Implement other user profile page and Add past reservations to profile

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import CompleteProfilePage from './components/home/Topbar/Menu/CompleteProfilePa
 import RegisterPage from './components/home/Topbar/Menu/RegisterPage';
 import MyReservations from './routes/MyReservations';
 import MyReviews from './routes/MyReviews';
+import OtherUserProfilePage from './routes/OtherUserProfilePage';
 import ProfileEdit from './routes/ProfileEdit';
 import ProfilePage from './routes/ProfilePage';
 import ReservationDetails from './routes/ReservationDetails';
@@ -30,6 +31,7 @@ export const App = () => {
           <Route path="/redirect" element={<Redirect />} />
           <Route path="/hosting" element={<Hosting />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/:userId" element={<OtherUserProfilePage />} />
           <Route path="/profile/edit" element={<ProfileEdit />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/MyReservations" element={<MyReservations />} />
